b58af29006bf727bb797b490aefa45ce1d23c7d1
luhluh-17 <castro.ralph17@gmail.com>
2022-09-27 15:12:10 +0800

    add category model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  def require_user_logged_in!
+    redirect_to new_user_session_path, alert: 'You must be signed in to do that.' unless user_signed_in?
+  end
 end
