2be58f7a98646d5344ab1ff07d187d406f815636
luhluh-17 <castro.ralph17@gmail.com>
2022-09-27 18:15:17 +0800

    add styling to categories

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,10 @@
 module CategoriesHelper
+  def random_text_bg
+    array = %w[success warning info]
+    "badge text-bg-#{array.sample}"
+  end
+
+  def count_task(category)
+    Task.where(category_id: category.id).count
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :tasks
   resources :categories
   devise_for :users
-  root 'categories#index'
+  root 'pages#index'
 end
