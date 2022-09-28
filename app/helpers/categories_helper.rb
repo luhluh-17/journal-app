f65596c3b0b71c3c45cdaa811f7a1c38880b98e4
luhluh-17 <castro.ralph17@gmail.com>
2022-09-28 18:52:47 +0800

    remove ununsed helper

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,9 +1,4 @@
 module CategoriesHelper
-  def random_text_bg
-    array = %w[success warning info]
-    "badge text-bg-#{array.sample}"
-  end
-
   def count_task(category)
     Task.where(category_id: category.id).count
   end
