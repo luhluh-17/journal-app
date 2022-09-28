8b3ac6958d7ba385a9476081a1566a7d12f7b19a
luhluh-17 <castro.ralph17@gmail.com>
2022-09-28 12:35:46 +0800

    fix dropdown menu to show other users data

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,7 +1,9 @@
 module TasksHelper
   def set_select_option
     array = []
-    Category.all.each { |category| array << [category.title, category.id] }
+    current_user.categories.each do |category|
+      array << [category.title, category.id]
+    end
     array
   end
 end
