53ae34b71ceeb4c696c68503b6834ff2093b64d4
luhluh-17 <castro.ralph17@gmail.com>
2022-11-15 08:10:07 +0800

    add logout functionality in sidebar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(_resource)
+    root_path
+  end
+
+  def after_sign_out_path_for(_resource)
+    new_user_session_path
+  end
 end
