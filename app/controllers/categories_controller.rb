fe47f32330b16c204bbb66c8e4fd3c0240a256da
luhluh-17 <castro.ralph17@gmail.com>
2022-11-15 09:21:47 +0800

    create categorie controller

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,48 @@
+class CategoriesController < ApplicationController
+  before_action :set_category, only: %i[show edit update destroy]
+
+  def index
+    @categories = current_user.categories
+  end
+
+  def show; end
+
+  def new
+    @category = Category.new
+  end
+
+  def edit; end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to categories_path, notice: "#{@category.title} was added"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @category.update(category_params)
+      redirect_to @category, notice: "#{@category.title} was renamed"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @category.destroy
+    redirect_to categories_url, notice: "#{@category.title} was deleted"
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:title, :color, :user_id)
+  end
+end
